565e8e27a975c250a3fe30a7564beea377c79007
Matthew John Ragas <167336812+MatthewJRagas@users.noreply.github.com>
2024-04-18 22:46:35 -0500

    Bullets Destroy when outside camera view

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour
 {
+    public Camera cam;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,18 @@ public class BulletBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 boundaryCheck = cam.WorldToViewportPoint(transform.position);
+        if(boundaryCheck.x < 0 || boundaryCheck.x > 1)
+        {
+            Destroy(gameObject);
+        }
+        else if(boundaryCheck.y < 0 || boundaryCheck.y > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+
+        }
     }
 }
